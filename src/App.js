61aff00d8ae27d10e576b5e7c30e60958dd1281a
honey-2020 <57524774+honey-2020@users.noreply.github.com>
2021-02-24 11:11:45 +0100

    add env variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const message = process.env.REACT_APP_MESSAGE || "Manca un dato...";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {message}
         </p>
         <a
           className="App-link"
